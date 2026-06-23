a4895fa0182947f2254cf50612444f86b51726ba
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-06-23 14:22:27 +0300

    Adjust html structure and add styling to everything

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -2,7 +2,7 @@ import GeneralForm from "./GeneralForm";
 import EducationForm from "./EducationForm";
 import ExperienceForm from "./ExperienceForm";
 import { useState } from "react";
-import "../styles/forms.css";
+import "../styles/cv.css";
 
 function CV() {
   const [data, setData] = useState({
@@ -25,54 +25,68 @@ function CV() {
 
   return (
     <>
-      <section className="form">
-        <div className="general-form">
-          <GeneralForm
-            value={{ firstName: data.firstName, lastName: data.lastName }}
-            onChange={handleChange}
-          ></GeneralForm>
-        </div>
-        <div className="education-form">
-          <EducationForm
-            value={{
-              degree: data.degree,
-              university: data.university,
-              startDate: data.startDate,
-              endDate: data.endDate,
-            }}
-            onChange={handleChange}
-          ></EducationForm>
-        </div>
-        <div className="experience-form">
-          <ExperienceForm
-            value={{
-              experience: data.experience,
-            }}
-            onChange={handleChange}
-          ></ExperienceForm>
-        </div>
-      </section>
-      <main className="resume">
-        <section className="personal-details">
-          <div>
-            <h2>Personal Details</h2>
-            <p>First Name: {data.firstName}</p>
-            <p>Last Name: {data.lastName}</p>
-            <p>Email: {data.email}</p>
+      <main className="app">
+        <article className="resume">
+          <div className="resume-content">
+            <section className="personal-details">
+              <div>
+                <div className="titles">
+                  {" "}
+                  <h2>Personal Details</h2>
+                </div>
+
+                <p>First Name: {data.firstName}</p>
+                <p>Last Name: {data.lastName}</p>
+                <p>Email: {data.email}</p>
+              </div>
+            </section>
+            <section className="education">
+              <div>
+                <div className="titles">
+                  {" "}
+                  <h2>Education</h2>
+                </div>
+                <p>Degree: {data.degree}</p>
+                <p>University: {data.university}</p>
+                <p>Start Date: {data.startDate}</p>
+                <p>End Date: {data.endDate}</p>
+              </div>
+            </section>
+            <section className="experience">
+              <div className="titles">
+                {" "}
+                <h2>Experience</h2>
+              </div>
+              <p>{data.experience}</p>
+            </section>
           </div>
-        </section>
-        <section className="education">
-          <div>
-            <h2>Education</h2>
-            <p>Degree: {data.degree}</p>
-            <p>University: {data.university}</p>
-            <p>Start Date: {data.startDate}</p>
-            <p>End Date: {data.endDate}</p>
+        </article>
+        <section className="forms-panel">
+          <div className="general-form">
+            <GeneralForm
+              value={{ firstName: data.firstName, lastName: data.lastName }}
+              onChange={handleChange}
+            ></GeneralForm>
           </div>
-        </section>
-        <section className="experience">
-          <h2>Experience</h2>
-          <p>{data.experience}</p>
+          <div className="education-form">
+            <EducationForm
+              value={{
+                degree: data.degree,
+                university: data.university,
+                startDate: data.startDate,
+                endDate: data.endDate,
+              }}
+              onChange={handleChange}
+            ></EducationForm>
+          </div>
+          <div className="experience-form">
+            <ExperienceForm
+              value={{
+                experience: data.experience,
+              }}
+              onChange={handleChange}
+            ></ExperienceForm>
+          </div>
         </section>
       </main>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
 import CV from "./components/CV";
 
 createRoot(document.getElementById("root")).render(
